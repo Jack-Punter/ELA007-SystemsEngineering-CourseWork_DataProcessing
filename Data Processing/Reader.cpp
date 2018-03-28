ba2b46a7522cbd97e3a638c12485cbe0f9c99e01
TarriestPython <TarriestPython@users.noreply.github.com>
2018-03-28 23:01:18 +0100

    Initial commint. enough code to read the data files into a vector of structs containing the longitude latitue, bearing and time of each reading.

--- Data Processing/Reader.cpp
+++ Data Processing/Reader.cpp
@@ -1,11 +1,14 @@
-#include "Reader.h"
 #include <iostream>
 #include <assert.h>
 #include <ostream>
+#include "Reader.h"
 
 std::vector<fileData> Reader(const std::string file)
 {
 	std::vector<fileData> data;
+	//Preallocate 75 entries in the vector
+	data.reserve(75);
+
 	//Open an input filestream and checked that the file was successfully opened
 	std::ifstream in(file);
 	if (!in)
@@ -25,9 +28,12 @@ std::vector<fileData> Reader(const std::string file)
 
 		//Read a line from the file
 		in.getline(&strLong[0], 64, ',');
-		in.getline(&strLat[0], 64, ',');
-		in.getline(&strBearing[0], 64, ',');
-		in.getline(&strTime[0], 64, '\n');
+		if (in.good())
+		{
+			in.getline(&strLat[0], 64, ',');
+			in.getline(&strBearing[0], 64, ',');
+			in.getline(&strTime[0], 64, '\n');
+		}
 
 		//If the file stream is still in a good state (not encountered an EOF or error)
 		//convert the loaded strings into number values and create a fileData object to put into
@@ -51,19 +57,23 @@ std::vector<fileData> Reader(const std::string file)
 
 	//Something is really bad like the file was lost after opening (storage drive was removed during reading)
 	if (in.bad())
+	{
 		throw std::runtime_error("Soemthing Got FUCKED");
-	//This currently will be true when we reach the end of the file (possibly due to multiple reads after EoF) 
+	}
+	//If the eof has been reached we can just return the data
+	else if (in.eof())
+	{
+		return data;
+	}
+	//If we stopped and none of bad() and eof() there was a fail()
 	else if (in.fail())
 	{
-		std::cout << "Some kind of fail?\nPossibly due to end of last line was end of stream.\n";
+		throw std::runtime_error("Some kind of fail?\nPossibly due to end of last line was end of stream.\n");
 	}
 }
 
 //Insertion operator to allow std::cout to accept fileData structs
 std::ostream& operator<< (std::ostream& cout, const fileData& data)
 {
-	cout << "Longitude; " << data.longitude;
-	cout << "\nLatitude: " << data.latitude;
-	cout << "\nBearing: " << data.bearing;
-	cout << "\nTime: " << data.time << std::endl;
+	return cout << "Longitude; " << data.longitude << "\nLatitude: " << data.latitude << "\nBearing: " << data.bearing << "\nTime: " << data.time << std::endl;
 }
--- Data Processing/Source.cpp
+++ Data Processing/Source.cpp
@@ -12,10 +12,18 @@ R = 22.24 / 13.46
 
 void main()
 {
-
+	std::vector<fileData> sensor1;
+	std::vector<fileData> sensor2;
 	//Read the 2 data files for the first run into the data files
-	auto sensor1 = Reader("G08_RUN-1.CSV");
-	auto sensor2 = Reader("G14_RUN-1.CSV");
+	try
+	{
+		sensor1 = Reader("G08_RUN-1.CSV");
+		sensor2 = Reader("G14_RUN-1.CSV");
+	}
+	catch (std::runtime_error e)
+	{
+		std::cout << e.what() << '\n';
+	}
 
 	//Print out the last element of the data to check it got all of the file
 	std::cout << sensor1.back() << std::endl;
