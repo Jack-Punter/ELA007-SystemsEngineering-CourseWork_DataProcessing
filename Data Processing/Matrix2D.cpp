c3cbda3f86ded44beab5eb1aea5565b7a260c985
TarriestPython <TarriestPython@users.noreply.github.com>
2018-03-30 01:30:18 +0100

    Added a static member function to Matrix2D to return a clockwise rotation matrix of angle in degrees

--- Data Processing/Matrix2D.cpp
+++ Data Processing/Matrix2D.cpp
@@ -1,4 +1,6 @@
+#include <cmath>
 #include "Matrix2D.h"
+#define PI 3.14159265358979323846
 
 Matrix2D::Matrix2D(double r0c0, double r0c1, double r1c0, double r1c1)
 	:
@@ -29,6 +31,17 @@ double Matrix2D::Det() const
 Matrix2D Matrix2D::Inverse() const
 {
 	return 1.0 / Det() * Matrix2D(r1c1, -r0c1, -r1c0, r0c0);
+}
+
+Matrix2D Matrix2D::Rotation(double angle)
+{
+	double theta = angle * PI / 180.0;
+
+	double c = cos(theta);
+	double s = sin(theta);
+
+	return { c, s,
+			-s, c };
 }
 
 Matrix2D operator*(double lhs, const Matrix2D& rhs)
